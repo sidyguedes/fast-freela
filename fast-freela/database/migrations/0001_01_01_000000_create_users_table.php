08d05ec0816be049334431d4e2a6fe62ce660a84
Sidgley Guedes <sidgley_guedes@hotmail.com>
2024-10-29 21:54:01 -0300

    feat: create migrations users and projects

--- fast-freela/database/migrations/0001_01_01_000000_create_users_table.php
+++ fast-freela/database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,9 +15,8 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('avatar')->nullable();
+            $table->unsignedTinyInteger('rating')->default(0);
             $table->timestamps();
         });
 
